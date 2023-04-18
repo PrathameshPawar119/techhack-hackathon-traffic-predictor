eaa9534741e6f098fdd9f4df668441c357cf57e7
Prathamesh Pawar <cm.b26.prathameshpawar@gmail.com>
2023-04-19 02:48:04 +0530

    Api completed

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,14 +5,17 @@ import tensorflow as tf
 from tensorflow.keras.models import load_model
 from fastapi.middleware.cors import CORSMiddleware
 import numpy as np
+from typing import List
 
 
 app = FastAPI()
 
 
-class Prediction(BaseModel):
-    time_steps: int
-    data: list[list[float]]
+# class Prediction(BaseModel):
+#     time_steps: int
+#     data: list[list[float]]
+class Data(BaseModel):
+    data: List[List[float]]
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,30 +25,18 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-
-@app.on_event("startup")
-def load_model():
-    global model
-    # Load the saved model
-    model = load_model("../model.h5")
+model = load_model("model.h5")
 
 
 @app.post("/predict")
-async def predict_traffic(p: Prediction):
-    # Transform the input data to numpy array
-    data = np.array(p.data)
+async def predict(data: Data):
+    # convert the input data to a numpy array and reshape it
+    x = np.array(data.data).reshape(-1, 32, 1)
+    # make predictions using the model
+    y_pred = model.predict(x)
+    # return the predicted values
+    return {"predictions": y_pred.tolist()}
 
-    # Reshape the data for input to the model
-    data = np.reshape(data, (data.shape[0], p.time_steps, data.shape[1]))
-
-    # Make the prediction using the loaded model
-    pred = model.predict(data)
-
-    # Convert the predicted values to a list
-    pred_list = pred.tolist()
-
-    # Return the predicted values
-    return {"prediction": pred_list}
 
 
 if __name__ == "__main__":
