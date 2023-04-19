c1b5f0248b42983361263a634e9f570898e2bacf
Prathamesh Pawar <cm.b26.prathameshpawar@gmail.com>
2023-04-19 10:52:32 +0530

    Index page (yet incomplete)

--- frontend/components/Post/Card.vue
+++ frontend/components/Post/Card.vue
@@ -5,67 +5,27 @@
     theme="light"
     max-width="540"
   >
-    <!-- <template v-slot:prepend>
-      <v-icon size="x-large"></v-icon>
-    </template> -->
     <v-img
       cover
       height="250"
       style="margin-top: 0px; cursor: pointer;"
-      @click="navigateTo(`/${post.id}/post`)"
-      :src="post.image ? post.image : 'https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'"
+      :src="'https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'"
     ></v-img>
     <div class="content" style="text-align: start;">
       <v-card-text class="text-h6 mt-2">
         <div class="chips mt-0 px-1" style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-between;">
-          <div>
-            <v-chip>Project Completed</v-chip>
-            <v-chip>Painting</v-chip>
-          </div>
-          <p style="font-size: 14px;" class="text-h7">{{ formatDate(post.created_at).formattedDate}}</p>
+          <p style="font-size: 14px;" class="text-h7">Average number of vehichles on Location </p>
         </div>
       </v-card-text>
-      <v-card-text class="py-1" style="font-size: 18px; cursor: pointer;"   @click="navigateTo(`/${post.id}/post`)">
-          {{ post.content }} 
-        <div @click="alterContentLen" style="display: inline;">...{{readMore ? 'less' : 'more'}}</div>
-      </v-card-text>
     </div>
-
-    <v-card-actions>
-      <v-list-item class="w-100">
-        <template v-slot:prepend>
-            <v-avatar
-            @click="navigateTo(`/${post.creator}/social`)"
-            class="cursor-pointer"
-            color="grey-darken-3"
-            image="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-            ></v-avatar>
-        </template>
-        
-          <v-list-item-title style="text-align: start; cursor: pointer;" @click="navigateTo(`/${post.creator}/social`)">
-            {{post.name}}
-          </v-list-item-title>
-          <v-list-item-subtitle style="text-align: start;">{{ post.title }}</v-list-item-subtitle>
-
-        <template v-slot:append>
-          <div class="justify-self-end">
-            <v-icon class="me-1" icon="$heart2"></v-icon>
-            <span class="subheading me-2">{{post.likes}}</span>
-            <span class="me-1">·</span>
-            <v-icon class="me-1" icon="$share"></v-icon>
-            <span class="subheading">45</span>
-          </div>
-        </template>
-      </v-list-item>
-    </v-card-actions>
   </v-card>
 </template>
 
 <script setup>
 
 const props = defineProps({
-    post:{
-        type:Object,
+    pred:{
+        type:Number,
         required:true
     }
 })
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,26 +1,115 @@
 <template>
     <v-container class="text-center">
       <section v-if="pending">
-        <loading message="Loadng Posts..." />
+        <loading message="Loadng Todays traffic..." />
       </section>
       <section v-else>
         <div class="displaySection">
-          <h1 class="text-h2 font-weight-bold">Today's traffic at All Places</h1>
+          <h2 class="text-h3">Today's traffic at All Places</h2>
+        </div>
+        <div>
+        </div>
+        <div class="grid mt-6">
+            <v-card
+              class="mx-4 my-4 card-glass"
+              color="purple-darken-4"
+              theme="light"
+              max-width="540"
+              v-for="(item, i) in predictions" :key="i"
+            >
+              <v-img
+                cover
+                height="250"
+                style="margin-top: 0px; cursor: pointer;"
+                :src="'https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'"
+              ></v-img>
+              <div class="content" style="text-align: start;">
+                <v-card-text class="text-h6 mt-2">
+                  <div class="chips mt-0 px-1" style="display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-between;">
+                    <p style="font-size: 14px;" class="text-h7">Average number of vehichles on Location 1 is  {{ ~~(item * 100) }}</p>
+                    <h2 class="text-h4"></h2>
+                  </div>
+                </v-card-text>
+              </div>
+            </v-card>
         </div>
       </section>
     </v-container>
 </template>
 
 <script setup>
-import { useCitiesStore } from "@/stores/cities";
-import { storeToRefs } from "pinia";
+// import { useCitiesStore } from "@/stores/cities";
+// import { storeToRefs } from "pinia";
+import Chart from 'chart.js/auto'
 
 const {$useFetchApi:useFetchApi} = useNuxtApp();
-const cities = useCitiesStore();
-const { defaultCity, cookieCity } = storeToRefs(cities);
+// const cities = useCitiesStore();
+// const { defaultCity, cookieCity } = storeToRefs(cities);
 const route = useRoute();
 const reactiveQuery = computed(() => route.query);
-const drawer = ref(false);
+const predictions = ref([]);
+
+
+
+// async function fetchPreds() {
+//   try {
+//     const response = await $axios.post('http://localhost:5000/predict_all_junction', {
+//       date: "2015-01-11",
+//     })
+//     predictions.value = response.data.predictions
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+const { data, pending, error, refresh } = await useFetch('http://localhost:5000/predict_all_junction', {
+  method: "POST",
+  body:{
+    date:"2015-01-11"
+  },
+  query:reactiveQuery
+})
+watch(data, ()=>{
+  predictions.value = data.value.predictions;
+})
+predictions.value[0] = data.value.predictions[0][0][0];
+predictions.value[1] = data.value.predictions[1][0][0];
+predictions.value[2] = data.value.predictions[2][0][0];
+predictions.value[3] = data.value.predictions[3][0][0];
+
+console.log("predications -----", predictions.value);
+watch(reactiveQuery, ()=>{
+  refresh();
+})
+
+
+
+// (async function () {
+//   const data =  [
+//     {'location 1':predictions.value[0]},
+//      { 'location 2': predictions.value[1] },
+//       { 'location 3': predictions.value[2] },
+//       { 'location 4': predictions.value[3] },
+
+//   ];
+
+//   new Chart(
+//     document.getElementById('acquisitions'),
+//     {
+//       type: 'bar',
+//       data: {
+//         labels: data.map(row => row.year),
+//         datasets: [
+//           {
+//             label: 'Acquisitions by year',
+//             data: data.map(row => row.count)
+//           }
+//         ]
+//       }
+//     }
+//   );
+// })();
+
 
 
 useHead({
@@ -33,9 +122,9 @@ useHead({
 </script>
 
 <style scoped>
-.displaySection{
+.grid{
   display: grid;
-  grid-template-columns: 1fr 4fr 2fr;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-template-rows: 1fr;
 }
   .rightSection, .leftSection{
@@ -45,9 +134,9 @@ useHead({
 
 @media screen and (max-width: 660px){
   .displaySection{
-    display: grid;
+    /* display: grid;
     grid-template-columns: 1fr;
-    grid-template-rows: 1fr;
+    grid-template-rows: 1fr; */
   }
 
   .rightSection, .leftSection{
